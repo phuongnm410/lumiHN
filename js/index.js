150608ec569a0658605c4b474512b6294f423365
Nguyen Minh Phuong <nmp24141@gmail.com>
2024-07-26 16:55:26 +0700

    feat: build stucture

--- js/index.js
+++ js/index.js
@@ -1,3 +1,28 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // $('#formModal').modal('show');
+    document.querySelector('#formModal').addEventListener("shown.bs.modal", function (e) {
+        $('#formModal').find(".inner").addClass("go");
+    });
+    $('#formModal').on('submit', function(event) {
+        event.preventDefault();
+        $('#formModal').modal('hide');
+        $('#thanksModal').modal('show');
+        setTimeout(() => {
+            $('#thanksModal').modal('hide');
+        }, 5000);
+    });
+
+    $('#thanksModal').on('shown.bs.modal', function () {
+        fullpage_api.setAllowScrolling(false);
+    });
+
+    // Enable fullPage.js scrolling when modal is closed
+    $('#thanksModal').on('hidden.bs.modal', function () {
+        fullpage_api.setAllowScrolling(true);
+    });
+});
+
+
 
 var swiper =new Swiper(".home-slide", {
     loop: true,
@@ -19,9 +44,9 @@ var swiper =new Swiper(".home-slide", {
 function methodPost(api, data) {
 	fetch(api, {
 		method: 'POST',
-        headers: {
-            'Content-Type': 'application/json; charset=UTF-8',
-        },
+        // headers: {
+        //     'Content-Type': 'application/json; charset=UTF-8',
+        // },
 		body: JSON.stringify(data),
 	})
 	.then((response) => console.log(response))
@@ -34,6 +59,18 @@ formRegister.addEventListener("submit", (event)=>{
     event.preventDefault();
 
     const formData = new FormData(formRegister);
+    const data = {};
+    
+    formData.forEach((value, key) => {
+      data[key] = value;
+    });
+    methodPost("https://api-kp.vdns.app/api/v1/customers", data);
+})
+let formRegisterPopup = document.querySelector("#form_register_popup");
+formRegisterPopup.addEventListener("submit", (event)=>{
+    event.preventDefault();
+
+    const formData = new FormData(formRegisterPopup);
     const data = {};
     
     formData.forEach((value, key) => {
